e4262516bc6b6940a77b3d2651307026bbb234e4
Nepokornyi <vadymkin@gmail.com>
2026-01-08 22:12:12 +0100

    simplify ApiGateway to verify apiKey

--- app/components/ApiGateway.tsx
+++ app/components/ApiGateway.tsx
@@ -1,70 +1,52 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useApiKey } from '@/context/ApiKeyContext'
+import { useState } from 'react'
 
 export const ApiGateway = () => {
+    const { apiKey, setApiKey, ready } = useApiKey()
     const [keyInput, setKeyInput] = useState('')
-    const [APIKey, setAPIKey] = useState<string | null>(null)
-    const [ready, setReady] = useState(false)
 
-    useEffect(() => {
-        setAPIKey(localStorage.getItem('gw2_api_key'))
-        setReady(true)
-    }, [])
+    if (!ready || apiKey) return null
 
-    useEffect(() => {
-        if (!APIKey) return
+    const handleSave = async () => {
+        try {
+            const res = await fetch('/api/verify', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ apiKey: keyInput }),
+            })
 
-        const verifyKey = async () => {
-            try {
-                const res = await fetch('/api/verify', {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify({ apiKey: APIKey }),
-                })
+            if (!res.ok) throw new Error('Invalid key')
 
-                if (!res.ok) throw new Error('Invalid key')
+            setApiKey(keyInput)
 
-                const data = await res.json()
-                console.log('Valid key data: ', data)
-            } catch (err) {
-                console.error('Request failed', err)
-            }
+            const data = await res.json()
+            console.log('Valid key data: ', data)
+        } catch (err) {
+            console.error('Request failed', err)
         }
-
-        verifyKey()
-    }, [APIKey])
-
-    const handleSave = (key: string) => {
-        localStorage.setItem('gw2_api_key', key)
-        setAPIKey(key)
     }
 
-    if (!ready) return null
-
     return (
-        <>
-            {!APIKey && (
-                <div>
-                    <div className="flex gap-5">
-                        <input
-                            className="bg-white text-black border-none"
-                            value={keyInput}
-                            onChange={(e) => setKeyInput(e.target.value)}
-                            placeholder="xxxxxxxx-xxxx-xxxx"
-                            type="text"
-                            name="api"
-                            id="api"
-                        />
-                        <button
-                            className="border border-white p-1.5"
-                            onClick={() => handleSave(keyInput)}
-                        >
-                            Add Key
-                        </button>
-                    </div>
-                </div>
-            )}
-        </>
+        <div>
+            <div className="flex gap-5">
+                <input
+                    className="bg-white text-black border-none"
+                    value={keyInput}
+                    onChange={(e) => setKeyInput(e.target.value)}
+                    placeholder="xxxxxxxx-xxxx-xxxx"
+                    type="text"
+                    name="api"
+                    id="api"
+                />
+                <button
+                    className="border border-white p-1.5"
+                    onClick={handleSave}
+                >
+                    Add Key
+                </button>
+            </div>
+        </div>
     )
 }
